62466245109ba4edead18f0ee376162b9fd7d739
Antoine Albertelli <antoine@antoinealb.net>
2016-09-11 14:07:04 +0200

    Export an API for protobuf interface

--- examples/protobuf/demo.c
+++ examples/protobuf/demo.c
@@ -3,28 +3,12 @@
 #include <stdio.h>
 #include <pthread.h>
 #include "../../messagebus.h"
+#include "../../messagebus_protobufs.h"
 #include "../posix/port.h"
-#include <pb.h>
-#include <pb_encode.h>
-#include <pb_decode.h>
 #include "simple.pb.h"
 
 messagebus_t bus;
 #define TOPIC_NAME "test"
-
-#define PB_PUBLISH(topic, msg, type) do { \
-    uint8_t buffer[type##_size]; \
-    pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer)); \
-    pb_encode(&stream, type##_fields, msg); \
-    messagebus_topic_publish(topic, buffer, sizeof(buffer)); \
-    } while (0);
-
-#define PB_WAIT(topic, msg, type) do { \
-    uint8_t buffer[type##_size]; \
-    messagebus_topic_wait(topic, &buffer, sizeof buffer); \
-    pb_istream_t stream = pb_istream_from_buffer(buffer, sizeof(buffer)); \
-    pb_decode(&stream, type##_fields, msg); \
-    } while(0);
 
 
 static void* producer(void *p)
@@ -33,7 +17,8 @@ static void* producer(void *p)
     SimpleMessage msg;
     int producer_number = (int)p;
 
-    printf("[publisher %d] waiting for topic myint\n", producer_number);
+    printf("[publisher %d] waiting for topic \"%s\"\n",
+            producer_number, TOPIC_NAME);
 
     msg.counter = 0;
 
@@ -43,7 +28,7 @@ static void* producer(void *p)
         printf("[publisher %d] writing %d on topic %s\n",
                 producer_number, msg.counter, topic->name);
 
-        PB_PUBLISH(topic, &msg, SimpleMessage);
+        MESSAGEBUS_PB_PUBLISH(topic, &msg, SimpleMessage);
 
         msg.counter += 1;
 
@@ -59,12 +44,13 @@ static void *consumer(void *p)
     int consumer_number = (int)p;
     SimpleMessage msg;
 
-    printf("[consumer %d] waiting for topic myint\n", consumer_number);
+    printf("[consumer %d] waiting for topic \"%s\"\n",
+            consumer_number, TOPIC_NAME);
 
     while (1) {
         topic = messagebus_find_topic_blocking(&bus, TOPIC_NAME);
 
-        PB_WAIT(topic, &msg, SimpleMessage);
+        MESSAGEBUS_PB_WAIT(topic, &msg, SimpleMessage);
 
         printf("[consumer %d] read %d on topic %s\n",
                consumer_number, msg.counter, topic->name);
